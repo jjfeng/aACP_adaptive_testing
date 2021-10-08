1772ec61e2909d7d875cfe6e84c2fb61267879c3
jjfeng <jean.feng@gmail.com>
2021-10-08 14:25:08 -0700

    cleaning

--- code/create_modeler.py
+++ code/create_modeler.py
@@ -17,8 +17,8 @@ from modelers import *
 def parse_args():
     parser = argparse.ArgumentParser(description="run simulation")
     parser.add_argument("--seed", type=int, default=0, help="seed")
-    parser.add_argument("--simulation", type=str, default="online_fixed")
-    parser.add_argument("--model-type", type=str, default="Logistic")
+    parser.add_argument("--simulation", type=str, default="online", choices=["adversary", "online"])
+    parser.add_argument("--model-type", type=str, default="Logistic", help="only used by the online modeler")
     parser.add_argument("--min-var-idx", type=int, default=0)
     parser.add_argument("--preset-coef", type=float, default=0)
     parser.add_argument("--out-file", type=str, default="_output/model.pkl")
@@ -31,18 +31,12 @@ def main():
     np.random.seed(args.seed)
 
     # Create model
-    if args.simulation == "neldermead":
-        clf = NelderMeadModeler(min_var_idx=args.min_var_idx)
-    elif args.simulation == "adversary":
+    if args.simulation == "adversary":
         clf = AdversarialModeler(
             min_var_idx=args.min_var_idx, preset_coef=args.preset_coef
         )
     elif args.simulation == "online":
         clf = OnlineAdaptiveLearnerModeler(args.model_type, start_side_batch=False)
-    elif args.simulation == "online_bad":
-        clf = OnlineAdaptiveLearnerModeler(args.model_type, start_side_batch=True)
-    elif args.simulation == "online_fixed":
-        clf = OnlineLearnerFixedModeler(args.model_type)
     else:
         raise NotImplementedError("modeler missing")
 
