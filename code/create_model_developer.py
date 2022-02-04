17fa2c1d9ba1aef108b9e25eb43f7059d7eb14fc
Jean Feng <jean.feng@gmail.com>
2022-02-04 07:28:29 -0800

    remove old

--- code/create_model_developer.py
+++ code/create_model_developer.py
@@ -22,7 +22,7 @@ def parse_args():
     parser.add_argument("--alpha", type=float, default=0.1, help="assumed alpha for adaptive testing")
     parser.add_argument("--power", type=float, default=0.3, help="desired power for testing")
     parser.add_argument("--update-incr", type=float, default=0.1, help="how much the adversary perturbs things")
-    parser.add_argument("--simulation", type=str, default="online", choices=["adversary", "online_delta", "online_compare", "online_compare_adapt", "online_compare_restrict"])
+    parser.add_argument("--simulation", type=str, default="online_delta", choices=["adversary", "online_delta", "online_compare"])
     parser.add_argument("--hypo-tester", type=str, default="auc", choices=["log_lik", "auc", "accuracy"])
     parser.add_argument("--model-type", type=str, default="Logistic", choices=["Logistic", "GBT", "SelectiveLogistic"])
     parser.add_argument("--scratch-file", type=str, default="_output/scratch.txt")
@@ -56,19 +56,9 @@ def main():
             clf = OnlineAdaptLossModeler(hypo_tester, min_valid_dat_size=args.min_valid_dat_size, predef_alpha=args.alpha, power=args.power, se_factor=args.se_factor)
         elif args.model_type == "SelectiveLogistic":
             clf = OnlineFixedSelectiveModeler(args.model_type, target_acc=0.8, init_accept=0.4, incr_accept=0.05)
-    elif args.simulation == "online_compare_restrict":
-        clf = OnlineAdaptRestrictCompareModeler(hypo_tester, min_valid_dat_size=args.min_valid_dat_size, predef_alpha=args.alpha, power=args.power, se_factor=args.se_factor)
-    elif args.simulation == "online_delta_restrict":
-        clf = OnlineAdaptRestrictLossModeler(hypo_tester, min_valid_dat_size=args.min_valid_dat_size, predef_alpha=args.alpha, power=args.power, se_factor=args.se_factor)
     elif args.simulation == "online_compare":
         if args.model_type == "Logistic":
             clf = OnlineAdaptCompareModeler(hypo_tester, min_valid_dat_size=args.min_valid_dat_size, validation_frac=args.valid_frac, predef_alpha=args.alpha, power=args.power, se_factor=args.se_factor)
-    elif args.simulation == "online_compare_adapt":
-        if args.model_type == "Logistic":
-            clf = OnlineAdaptCompareSideModeler(hypo_tester, min_valid_dat_size=args.min_valid_dat_size, validation_frac=args.valid_frac, predef_alpha=args.alpha, power=args.power, se_factor=args.se_factor)
-    elif args.simulation == "online":
-        if args.model_type != "SelectiveLogistic":
-            clf = OnlineSensSpecModeler(args.model_type, min_valid_dat_size=args.min_valid_dat_size)
 
     if clf is None:
         raise NotImplementedError("modeler missing")
