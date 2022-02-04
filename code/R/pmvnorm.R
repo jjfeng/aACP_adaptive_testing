8be12c0e007e74f89cb92522366c23a453c6ed00
Jean Feng <jean.feng@gmail.com>
2022-02-04 07:29:57 -0800

    clean

--- code/R/pmvnorm.R
+++ code/R/pmvnorm.R
@@ -1,9 +1,9 @@
 #!/usr/bin/env Rscript
 # Solves for the threshold that will spend close to the desired alpha
-# in a sequential test
+# in a sequential test. The test creates upper bounds for the test statistics
 # Arg 1: csv file for covariance matrix
 # Arg 2: alpha to spend
-# Arg 3 and more: critical values for prior normalized statistics
+# Arg 3 and more: critical values for prior normal statistics
 # Stdout: the critical value for desired alpha spending
 
 library(mvtnorm)
@@ -15,12 +15,9 @@ corr_mat = data.matrix(corr_mat_df)
 corr_mat = unname(corr_mat)
 corr_mat = (corr_mat + t(corr_mat))/2
 alpha_spend = 10^as.numeric(args[2])
-#lower_prior = as.numeric(args[seq(3, length(args))])
 upper_prior = as.numeric(args[seq(3, length(args))])
 
 get_sequential_spend <- function(thres) {
-  #lower_all = c(lower_prior, -Inf)
-  #upper_all = c(rep(Inf, length(lower_prior)), thres)
   lower_all = c(rep(-Inf, length(upper_prior)), thres)
   upper_all = c(upper_prior, Inf)
   res = pmvnorm(lower=lower_all, upper = upper_all, sigma=corr_mat, maxpts=50000, abseps =0.00001)
